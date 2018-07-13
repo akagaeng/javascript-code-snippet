bb4488b2f2d8b34fb6aab26ccda5186b133bb635
akagaeng <akagaeng@gmail.com>
2018-07-13 10:36:23 +0900

    async patterns

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # javascript-code-snippet
 javascript code snippet (promise, async/await, ES6, ... )
+
+reference https://scotch.io/tutorials/javascript-promises-for-dummies
+
+번역중
--- async-patterns/callback.js
+++ async-patterns/callback.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 var options = {
-  encoding: 'utf8'
+    encoding: 'utf8'
 }
 
 var file1 = 'files/file1.txt';
@@ -34,32 +34,79 @@ var file3 = 'files/file3.txt';
 // 3. use promise (https://www.npmjs.com/package/promise)
 console.log('\n3. use promise (https://www.npmjs.com/package/promise)');
 
-var readFileFunc1 = function () {
-  fs.readFile(file1, options, (err, file1Data) => {
-    console.log(file1Data);
-  });
+var readFileFunc1 = function() {
+    setTimeout(function() {
+        fs.readFile(file1, options, (err, file1Data) => {
+            console.log(file1Data);
+        });
+    }, 500);
+    return new Promise();
 }
 
-var readFileFunc2 = function () {
-  fs.readFile(file2, options, (err, file2Data) => {
-    console.log(file2Data);
-  });
+var readFileFunc2 = function() {
+    setTimeout(function() {
+        fs.readFile(file2, options, (err, file2Data) => {
+            console.log(file2Data);
+        });
+    }, 300);
 }
 
-var readFileFunc3 = function () {
-  fs.readFile(file3, options, (err, file3Data) => {
-    console.log(file3Data);
-  });
+var readFileFunc3 = function() {
+    setTimeout(function() {
+        fs.readFile(file3, options, (err, file3Data) => {
+            console.log(file3Data);
+        });
+    }, 100);
 }
-readFileFunc1();
-readFileFunc2();
-readFileFunc3();
-// fs.readFile(file1, options, (err, file1Data) => {
-//   console.log(file1Data);
-//   fs.readFile(file2, options, (err, file2Data) => {
-//     console.log(file2Data);
-//     fs.readFile(file3, options, (err, file3Data) => {
-//       console.log(file3Data);
-//     });
-//   });
+
+// Promise.all([readFileFunc2(), readFileFunc1(), readFileFunc3()]).then(function () {}, function () {
 // });
+
+var promise = function() {
+    return new Promise(function(resolve, reject) {
+        resolve();
+    });
+};
+
+// readFileFunc1();
+// readFileFunc2();
+// readFileFunc3();
+
+// Promise.resolve()
+//   .then(function () {
+//     readFileFunc1();
+//     console.log('11111');
+//   })
+//   .then(function () {
+//     readFileFunc2();
+//     console.log('22222');
+//   })
+//   .then(function () {
+//     readFileFunc3();
+//     console.log('33333');
+//   });
+
+
+function delay(time) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(resolve, time);
+    });
+}
+
+delay(100)
+    .then(function STEP2() {
+        console.log("2번단계 100ms 후");
+        return delay(200);
+    })
+    .then(function STEP3() {
+        console.log("3번단계 300ms 후");
+        return delay(200);
+    })
+    .then(function STEP4() {
+        console.log("4번단계 400ms 후");
+        return delay(200);
+    })
+    .then(function STEP5() {
+        console.log("5번단계 500ms 후");
+        return delay(200);
+    })
